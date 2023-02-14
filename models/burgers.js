f4a53b1377d177fbafaf6204174035fa44da90a3
Alejandro <amendez1361@bths.edu>
2023-02-13 23:17:22 -0500

    added style sheet and index html

---
+++ models/burgers.js
@@ -0,0 +1,18 @@
+const burgers = [  {    id: 1,    name: 'Classic Cheeseburger',    price: 7.99,    ingredients: ['beef patty', 'cheddar cheese', 'lettuce', 'tomato', 'onion', 'pickles', 'special sauce'],
+    description: 'A classic cheeseburger with all the fixings.'
+  },
+  {
+    id: 2,
+    name: 'Bacon Cheeseburger',
+    price: 8.99,
+    ingredients: ['beef patty', 'bacon', 'cheddar cheese', 'lettuce', 'tomato', 'onion', 'pickles', 'special sauce'],
+    description: 'A juicy cheeseburger with crispy bacon.'
+  },
+  {
+    id: 3,
+    name: 'Veggie Burger',
+    price: 6.99,
+    ingredients: ['veggie patty', 'lettuce', 'tomato', 'onion', 'pickles', 'special sauce'],
+    description: 'A vegetarian option with a delicious veggie patty.'
+  }
+];
