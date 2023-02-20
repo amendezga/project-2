4fc933c83e4d896e58cb9888eb17c849a48d2aea
Alejandro <amendez1361@bths.edu>
2023-02-20 18:52:27 -0500

    added create route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const burgers = require('./models/burgers');
 const app = express();
 
 
 // middleware
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 // index
@@ -18,12 +21,30 @@ app.get('/menu', (req, res) => {
 })
 
 // new
+app.get('/new', (req, res) => {
+  res.render('new.ejs')
+})
 
 // delete
 
 // update
 
 // create
+app.post('/create', (req, res) => {
+    const { name, protein, greens, topping1, topping2, sauce } = req.body;
+  
+    const ingredients = [protein, greens, topping1, topping2, sauce];
+  
+    const price = 10.99;
+    const description = 'A classic cheeseburger with all the fixings.';
+    const id = burgers.length + 1;
+  
+    const newBurger = { id, name, ingredients, description, price };
+  
+    burgers.push(newBurger);
+  
+    res.redirect('/menu');
+  });
 
 // edit
 
