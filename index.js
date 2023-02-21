964c1b567f34dd38e53c63625485f9ca53c4d4e7
Alejandro <amendez1361@bths.edu>
2023-02-21 01:25:14 -0500

    added dotenv support

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ const bodyParser = require('body-parser');
 const burgers = require('./models/burgers');
 const methodOverride = require('method-override');
 const burgersController = require('./controllers/controller')
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
-
 
 // middleware
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
 app.use('/', burgersController);
 
+const PORT = process.env.PORT;
 
-
-
-
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(PORT, () => {
+  console.log('Server is listening on port ' + PORT);
 });
